595df680b98daa78dbe36e9bbadd73a79574ac4a
ireanuoluwa <moyin.olutola@gmail.com>
2023-04-25 22:06:17 -0700

    First commit

---
+++ backend.py
@@ -0,0 +1,258 @@
+import streamlit as st
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+from sklearn.metrics import classification_report
+import nltk
+import time
+from nltk.corpus import stopwords
+import seaborn as sns
+import numpy as np
+import string
+from sklearn.metrics import confusion_matrix
+from wordcloud import WordCloud
+from nltk.corpus import stopwords
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.metrics import roc_auc_score
+from sklearn.feature_extraction.text import TfidfVectorizer
+import plotly.graph_objects as go
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from nltk import word_tokenize
+import imblearn
+from imblearn.over_sampling import RandomOverSampler
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+from transformers import pipeline
+
+
+
+nltk.download("wordnet")
+porter = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
+nltk.download('stopwords')
+stopwords = stopwords.words('english')
+
+models = ("BernoulliNB",
+          "Logistic Regression",
+          "GradientBoostingClassifier",
+          "LinearSVC",
+          'Distilbert Pipeline')
+
+def load_dataset():
+    path = "data/amazon_reviews.csv"
+    return pd.read_csv(path)
+
+
+
+def get_wordnet_pos(treebank_tag):
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
+
+
+def model_evaluate(model,X_train, X_test, y_train, y_test):
+    y_pred = model.predict(X_test)
+    classification_dict = classification_report(y_test, y_pred,output_dict = True)
+    try:
+        Pr_train = model.predict_proba(X_train)[:,1]
+        Pr_test = model.predict_proba(X_test)[:,1]
+        
+        train_AUC = roc_auc_score(y_train,Pr_train)
+        test_AUC = roc_auc_score(y_test,Pr_test)
+    except AttributeError as e:
+        train_AUC = None
+        test_AUC = None
+    return classification_dict,train_AUC,test_AUC
+        #   "Train AUC",roc_auc_score(y_train,Pr_train),
+        #   "Test AUC", roc_auc_score(y_test,Pr_test))
+
+class LemmaTokenizer:
+    def __init__(self):
+        self.wnl = WordNetLemmatizer()
+    def __call__(self,doc):
+        tokens = word_tokenize(doc)
+        words_and_tags = nltk.pos_tag(tokens)
+        return [self.wnl.lemmatize(word,pos = get_wordnet_pos(tag)) \
+                 for word,tag in words_and_tags]
+
+
+
+def preprocessing(message):
+    Test_punc_removed = [word for word in message if word not in string.punctuation]
+    Test_punc_removed_join = ''.join(Test_punc_removed)
+    Test_punc_removed_join_clean = [word for word in Test_punc_removed_join.split() if word.lower() not in stopwords and word.lower().isalpha() and word is not None]
+    Test_punc_removed_join_clean = ' '.join(Test_punc_removed_join_clean)
+    return Test_punc_removed_join_clean
+
+
+
+
+
+def feature_engineering(data_df):
+    X = data_df['verified_reviews']
+    y = data_df['feedback']
+    over_sampler = RandomOverSampler(random_state=42)
+    X_res, y_res = over_sampler.fit_resample(X.values.reshape(-1, 1), y)
+    X_res = np.ravel(X_res)
+    X_res = pd.Series(X_res)
+    X_train, X_test, y_train, y_test = train_test_split(X_res, y_res,
+                                                        stratify=y_res, 
+                                                        test_size=0.25, random_state = 245)
+    vectorizer = CountVectorizer(tokenizer=LemmaTokenizer(), lowercase=False)
+    X_train = vectorizer.fit_transform(X_train)
+    X_test = vectorizer.transform(X_test)
+    return X_train, X_test, y_train, y_test
+
+@st.cache_resource
+def confusion_matrix_func(model_name,_X_train, _X_test, _y_train, _y_test):
+    with st.spinner('Generating Confusion Matrix:'):
+        time.sleep(1)
+        model = model_name()
+        model.fit(_X_train, _y_train)
+        predicted = model.predict(_X_test)
+        cm = confusion_matrix(_y_test,predicted)
+        fig, ax = plt.subplots()
+        sns.heatmap(cm, annot=True, cmap='Blues', fmt='g', ax=ax)
+
+        # set axis labels and chart title
+        ax.set_xlabel('Predicted labels')
+        ax.set_ylabel('True labels')
+        ax.set_title('Confusion Matrix')
+    
+        
+    st.success('Confusion Matrix generated!')
+
+    # display the chart in Streamlit
+    return st.pyplot(fig)
+
+
+@st.cache_data
+def reset_feature():
+    path = "C:/Users/moyin/OneDrive/Desktop/ASU/MFG 598/PROJECT/data/amazon_reviews.csv"
+    df = pd.read_csv(path)
+    df['verified_reviews'].replace(' ', np.nan, inplace=True)
+    df.dropna(subset=['verified_reviews'], inplace=True)
+    X = df['verified_reviews']
+    y = df['feedback']
+    over_sampler = RandomOverSampler(random_state=42)
+    X_res, y_res = over_sampler.fit_resample(X.values.reshape(-1, 1), y)
+    X_res = np.ravel(X_res)
+    X_res = pd.Series(X_res)
+    X_train, X_test, y_train, y_test = train_test_split(X_res, y_res,
+                                                    stratify=y_res, 
+                                                    test_size=0.25, random_state = 245)
+    vectorizer = CountVectorizer(tokenizer=LemmaTokenizer(), lowercase=False)
+    X_train = vectorizer.fit_transform(X_train)
+    X_test = vectorizer.transform(X_test)
+    return X_train, X_test, y_train, y_test,vectorizer
+
+
+@st.cache_resource
+def load_transformer():
+    sentiment_classifier = pipeline(
+    model="distilbert-base-uncased-finetuned-sst-2-english",
+    task="sentiment-analysis",
+    top_k=None,
+)
+    return sentiment_classifier
+    
+
+
+def predict_pipeline(model,X_train, X_test, y_train, y_test):
+    y_pred = model(X_test)
+    classification_dict = classification_report(y_test, y_pred,output_dict = True)
+    try:
+        Pr_train = model.predict_proba(X_train)[:,1]
+        Pr_test = model.predict_proba(X_test)[:,1]
+        
+        train_AUC = roc_auc_score(y_train,Pr_train)
+        test_AUC = roc_auc_score(y_test,Pr_test)
+    except AttributeError as e:
+        train_AUC = None
+        test_AUC = None
+    return classification_dict,train_AUC,test_AUC
+
+
+
+def predict_model(model_name,user_input,X_train,X_test,y_train,y_test,vectorizer,alpha=None,C=None,max_feat=None,n_estim=None,n_jobs=None,max_iterations=None,max_lr=None):
+        if model_name == BernoulliNB :
+            model = model_name(alpha=alpha)
+            model.fit(X_train, y_train)
+            result,train_AUC,test_AUC = model_evaluate(model,X_train, X_test, y_train, y_test)
+            test = pd.Series(user_input)
+            # vectorizer = backend.load_vectorizer()
+            pred_test = vectorizer.transform(test)
+            predictions = model.predict(pred_test)
+            
+
+
+            # loaded_pipe = backend.load_transformer()
+            # predictions = loaded_pipe.predict(user_input)
+            pred_to_label = {0: 'Negative', 1: 'Positive'}
+
+            # Make a list of user_text with sentiment.
+            data = []
+            for t, pred in zip(user_input, predictions):
+                data.append((t, pred, pred_to_label[pred]))
+        
+        elif model_name == LogisticRegression:
+            model = model_name(C = C,max_iter = max_lr,n_jobs = n_jobs)
+            model.fit(X_train, y_train)
+            result,train_AUC,test_AUC= model_evaluate(model,X_train, X_test, y_train, y_test)
+            test = pd.Series(user_input)
+            pred_test = vectorizer.transform(test)
+            predictions = model.predict(pred_test)
+            pred_to_label = {0: 'Negative', 1: 'Positive'}
+
+            # Make a list of user_text with sentiment.
+            data = []
+            for t, pred in zip(user_input, predictions):
+                data.append((t, pred, pred_to_label[pred]))
+        elif model_name == LinearSVC:
+            model = model_name(C = C,max_iter = max_iterations)
+            model.fit(X_train, y_train)
+            result,train_AUC,test_AUC= model_evaluate(model,X_train, X_test, y_train, y_test)
+            test = pd.Series(user_input)
+            pred_test = vectorizer.transform(test)
+            predictions = model.predict(pred_test)
+            pred_to_label = {0: 'Negative', 1: 'Positive'}
+
+            # Make a list of user_text with sentiment.
+            data = []
+            for t, pred in zip(user_input, predictions):
+                data.append((t, pred, pred_to_label[pred]))
+        else:
+            model = model_name(n_estimators=n_estim,max_features=max_feat)
+            model.fit(X_train, y_train)
+            result,train_AUC,test_AUC= model_evaluate(model,X_train, X_test, y_train, y_test)
+            test = pd.Series(user_input)
+            pred_test = vectorizer.transform(test)
+            predictions = model.predict(pred_test)
+            pred_to_label = {0: 'Negative', 1: 'Positive'}
+
+            # Make a list of user_text with sentiment.
+            data = []
+            for t, pred in zip(user_input, predictions):
+                data.append((t, pred, pred_to_label[pred]))
+        
+        return data,result,train_AUC,test_AUC
+
+
+
+
+
+
---
+++ streamlit_app.py
@@ -0,0 +1,240 @@
+#import libraries
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+import string
+import nltk
+import time
+from wordcloud import WordCloud
+import backend as backend
+from nltk.corpus import stopwords
+import plotly.graph_objects as go
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+import imblearn
+from imblearn.over_sampling import RandomOverSampler
+
+
+# Function to load reviews data
+@st.cache_data
+def load_reviews():
+    return backend.load_dataset()
+
+# Load reviews data
+df = backend.load_dataset()
+ 
+
+# Create streamlit app header
+st.header('Sentiment Analysis App')
+
+# Sidebar menu options
+Menu_Options = st.sidebar.selectbox('Required Task',('Data Visualization','Prediction'))
+
+# Data visualization menu options
+if Menu_Options == 'Data Visualization':
+    st.write("Choose any of the options on the side bar to perform Data Visualization")
+    Visualization_options = st.sidebar.selectbox('Data Visualization',('Word Cloud','Labels Analysis','Confusion Matrix'))
+
+    # Word cloud option
+    if Visualization_options == 'Word Cloud':
+        total = df['verified_reviews'].tolist()
+        total = ' '.join(total)
+        good_reviews = df[df['feedback'] == 1]
+        bad_reviews = df[df['feedback'] == 0]
+        good = good_reviews['verified_reviews'].tolist()
+        bad = bad_reviews['verified_reviews'].tolist()
+        good_str = ' '.join(good)
+        bad_str = ' '.join(bad)
+        emotion = st.sidebar.selectbox('Choose Sentiment',('Postive','Negative','Total'))
+
+        # Positive sentiment word cloud
+        if emotion == 'Postive':
+            fig,ax = plt.subplots()
+            plt.imshow(WordCloud().generate(good_str),interpolation='bilinear')
+            plt.axis('off')
+            st.pyplot(fig)
+
+        # Negative sentiment word cloud    
+        if emotion == 'Negative':
+            fig,ax = plt.subplots()
+            plt.imshow(WordCloud().generate(bad_str),interpolation='bilinear')
+            plt.axis('off')
+            st.pyplot(fig)
+
+        # Total word cloud    
+        if emotion == 'Total':
+            fig,ax = plt.subplots()
+            plt.imshow(WordCloud().generate(total),interpolation='bilinear')
+            plt.axis('off')
+            st.pyplot(fig)
+
+    # Labels Analysis option       
+    if Visualization_options == 'Labels Analysis':
+        df['variation'] = df['variation'].str.strip()
+        tuple_variations = tuple(set(df['variation']))
+        type_select = st.selectbox("Pick a model type",tuple_variations)
+        type_df = df[df['variation'] == type_select]
+
+        # group by feedback and count the number of occurrences
+        feedback_counts = type_df['feedback'].value_counts()
+
+        # create a bar chart using Plotly
+        fig = go.Figure()
+        fig.add_trace(go.Bar(x=['Negative Reviews'], y=[feedback_counts.get(0, 0)], name='Negative', marker=dict(color='red')))
+        fig.add_trace(go.Bar(x=['Positive Reviews'], y=[feedback_counts.get(1, 0)], name='Positive', marker=dict(color='green')))
+
+        # add chart title and axis labels
+        fig.update_layout(title='Feedback counts for Charcoal Fabric', xaxis_title='Feedback', yaxis_title='Count')
+
+        # display the chart in Streamlit
+        st.plotly_chart(fig)
+
+    # Confusion Matrix option  
+    if Visualization_options == 'Confusion Matrix':
+        visual_models_cm = st.sidebar.selectbox('Prediction using Models',('BernoulliNB','Logistic Regression','GradientBoostingClassifier','LinearSVC'))
+        plot_cm  = st.sidebar.button('Plot Confusion Matrix')
+
+        if plot_cm:
+            X_train, X_test, y_test, y_train = backend.feature_engineering(df)
+            model_list = [BernoulliNB,LogisticRegression,GradientBoostingClassifier,LinearSVC]
+
+            if visual_models_cm == 'Logistic Regression':
+                model_name = model_list[1]
+                backend.confusion_matrix_func(model_name,X_train, X_test, y_test, y_train)
+                
+            if visual_models_cm == 'GradientBoostingClassifier':
+                model_name = model_list[2]
+                backend.confusion_matrix_func(model_name,X_train, X_test, y_test, y_train)
+                
+            if visual_models_cm == 'LinearSVC':
+                model_name = model_list[3]
+                backend.confusion_matrix_func(model_name,X_train, X_test, y_test, y_train)
+
+                
+            if visual_models_cm == 'BernoulliNB':
+                model_name = model_list[0]
+                backend.confusion_matrix_func(model_name,X_train, X_test, y_test, y_train)
+            
+        else:
+            pass
+
+# After the prediction Button is pressed, what happens next
+if Menu_Options == 'Prediction':
+    st.write("Analyzing various reviews on an amazon product and predicting the sentiment. You can choose any model and tune the parameters for the best performance.")
+    Models_toggle = st.sidebar.selectbox('Prediction using Models',('BernoulliNB','Logistic Regression','GradientBoostingClassifier','LinearSVC', 'Distilbert Pipeline'))
+    
+    user_input = st.text_input('Enter the review','Enter text')
+    user_input = [user_input]
+    X_train, X_test, y_train, y_test,vectorizer = backend.reset_feature()
+
+    # Logistic Regression Model
+    if Models_toggle == backend.models[1]:
+        predict_sentiment  = st.sidebar.button('Predict Sentiment')
+        C = st.slider('C',0.0, 10.0, (1.0))
+        max_lr = st.slider('Maximum Features',0, 10000, (1000))
+        n_jobs =  st.slider('Number of Jobs',-2, 2, (-1))
+
+        if predict_sentiment:
+            with st.spinner('Evaluating model performance and sentiment analysis'):
+                progress_text = "Operation in progress. Please wait."
+                my_bar = st.progress(0, text=progress_text)
+
+                for percent_complete in range(100):
+                    time.sleep(0.1)
+                    my_bar.progress(percent_complete + 1, text=progress_text)
+
+                data,result,train_AUC,test_AUC = backend.predict_model(LogisticRegression,user_input,X_train,X_test,y_train, y_test,vectorizer,C=C,max_lr = max_lr, n_jobs = n_jobs)
+                labels = 'Sentiment'
+                values = data[0][2]
+                st.metric(label=labels, value=values, delta=None)
+                
+                st.subheader("Model Performance")
+                metric_data = pd.DataFrame(result)
+                st.table(metric_data)
+                st.write(f"ROC_AUC_Score (Train) {train_AUC}")
+                st.write(f"ROC_AUC_Score (Test) {test_AUC}")
+            st.success('Loaded')
+
+        else:
+            pass
+
+    # Gradient Boosting Classifier Model
+    if Models_toggle == backend.models[2]:
+        predict_sentiment  = st.sidebar.button('Predict Sentiment')
+        n_estim = st.slider('Number of Estimators',1, 100, (100))
+        max_feat = st.slider('Maximum Features',1, 100, (1))
+
+        if predict_sentiment:
+            data,result,train_AUC,test_AUC = backend.predict_model(GradientBoostingClassifier,user_input,X_train,X_test,y_train, y_test,vectorizer,n_estim=n_estim,max_feat = max_feat)
+            labels = 'Sentiment'
+            values = data[0][2]
+            st.metric(label=labels, value=values, delta=None)
+            
+            st.subheader("Model Performance")
+            metric_data = pd.DataFrame(result)
+            st.table(metric_data)
+            st.write(f"ROC_AUC_Score (Train) {train_AUC}")
+            st.write(f"ROC_AUC_Score (Test) {test_AUC}")
+        else:
+            pass
+
+    #LinearSVC    
+    if Models_toggle == backend.models[3]:
+        predict_sentiment  = st.sidebar.button('Predict Sentiment')
+        C = st.slider('C',1, 10, (1))
+        max_iterations = st.slider('Maximum Number of Iterations',1000, 20000, (1000))
+        if predict_sentiment:
+            data,result,train_AUC,test_AUC = backend.predict_model(LinearSVC,user_input,X_train,X_test,y_train, y_test,vectorizer,C = C,max_iterations = max_iterations)
+            labels = 'Sentiment'
+            values = data[0][2]
+            st.metric(label=labels, value=values, delta=None)
+            st.subheader("Model Performance")
+            metric_data = pd.DataFrame(result)
+            st.table(metric_data)
+            st.write(f"ROC_AUC_Score (Train) {train_AUC}")
+            st.write(f"ROC_AUC_Score (Test) {test_AUC}")
+        else:
+            pass
+
+    # BernoulliNB Model
+    if Models_toggle == backend.models[0]:
+        predict_sentiment  = st.sidebar.button('Predict Sentiment')
+        lm = st.slider('Alpha',0, 10, (1))
+        if predict_sentiment:
+            data,result,train_AUC,test_AUC = backend.predict_model(BernoulliNB,user_input,X_train,X_test,y_train, y_test,vectorizer,alpha=lm)
+           
+            labels = 'Sentiment'
+            values = data[0][2]
+            st.metric(label=labels, value=values, delta=None)
+        
+            st.subheader("Model Performance")
+            metric_data = pd.DataFrame(result)
+            st.table(metric_data)
+            st.write(f"ROC_AUC_Score (Train) {train_AUC}")
+            st.write(f"ROC_AUC_Score (Test) {test_AUC}")
+        else:
+            pass
+
+    #Distillbert Model    
+    if Models_toggle == backend.models[4]:  
+        predict_sentiment  = st.sidebar.button('Predict Sentiment')
+        transformer = backend.load_transformer()
+
+        if predict_sentiment:
+            data = transformer(user_input)
+            labels = 'Sentiment'
+            values = max([label for label in data[0]], key=lambda x: x['score'])['label']
+            st.metric(label=labels, value=values, delta=None)
+
+            # result,train_AUC,test_AUC = backend.predict_pipeline(transformer,X_train, X_test, y_train, y_test)
+            # metric_data = pd.DataFrame(result)
+            # st.table(metric_data)
+            # st.write(f"ROC_AUC_Score (Train) {train_AUC}")
+            # st.write(f"ROC_AUC_Score (Test) {test_AUC}")
+
+
